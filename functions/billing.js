7a3810df7b31dda0a53a31c3a9aa49c55cecdfaa
Tirtadwipa Manunggal <tirtadwipa.manunggal@gmail.com>
2020-01-04 21:00:09 +0700

    [UPDATE] Add billing API

---
+++ functions/billing.js
@@ -0,0 +1,24 @@
+const stripePackage = require('stripe')
+const { calculateCost } = require('./libs/billing')
+const { success, failure } = require('../libs/responses')
+
+export async function main (event, context) {
+  const { storage, source } = JSON.parse(event.body)
+  const amount = calculateCost(storage)
+  const description = 'Scratch charge'
+
+  // Load our secret key from the  environment variables
+  const stripe = stripePackage(process.env.stripeSecretKey)
+
+  try {
+    await stripe.charges.create({
+      source,
+      amount,
+      description,
+      currency: 'usd'
+    })
+    return success({ status: true })
+  } catch (e) {
+    return failure({ message: e.message })
+  }
+}
